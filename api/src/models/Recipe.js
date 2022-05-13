268cc27ba964222fdaa2a4b0035eaeb465d1c90d
riosgas <78388221+riosgas@users.noreply.github.com>
2022-05-12 23:15:15 -0300

    test

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -3,10 +3,30 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
+
   sequelize.define('recipe', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    summary: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    score: {
+      type: DataTypes.FLOAT
+    },
+    healthScore: {
+      type: DataTypes.FLOAT
+    },
+    instructions: {
+      type: DataTypes.TEXT,
+    }
   });
 };
